8e24d4ec8e3ce683f1cdbccd66a3b1bad031d7e7
y-yammt <y.yammt@gmail.com>
2016-07-28 22:30:49 +0900

    Added unit tests in util_test.cc

--- src/rnn/util_test.cc
+++ src/rnn/util_test.cc
@@ -6,7 +6,7 @@
 namespace {
   const unsigned int kRandomSeed = 0;
   const Real kMaxValue = 100;
-  const Real kEps = 0.0001;
+  const Real kEps = 0.01;
   const size_t kNumValues = 100000;
 
   std::pair<Real, Real> getMeanVariance(const size_t sz, const Real *values) {
@@ -35,27 +35,32 @@ TEST(UtilTest, InitNormal) {
   Real random_values[kNumValues];
   InitNormal(1.0, kNumValues, random_values);
   auto stat = getMeanVariance(kNumValues, random_values);
-  EXPECT_NEAR(0.0, stat.first, 1.0);
-  EXPECT_NEAR(1.0, stat.second, 1.0);
+  EXPECT_NEAR(0.0, stat.first, kEps);
+  EXPECT_NEAR(1.0, stat.second, kEps);
 }
 
 TEST(UtilTest, Clip) {
-  ASSERT_EQ(0, Clip(std::numeric_limits<Real>::quiet_NaN(), 1.0));
+  ASSERT_EQ(0.0, Clip(std::numeric_limits<Real>::signaling_NaN(), 1.0));
+  ASSERT_EQ(0.0, Clip(std::numeric_limits<Real>::quiet_NaN(), 1.0));
   ASSERT_EQ(-1.0, Clip(-2.0, 1.0));
   ASSERT_EQ(1.0, Clip(2.0, 1.0));
 }
 
 TEST(UtilTest, ShrinkMatrix) {
-  Eigen::Matrix<Real, 2, 2, Eigen::RowMajor> matrix;
-  matrix << std::numeric_limits<Real>::quiet_NaN(),
-      std::numeric_limits<Real>::infinity(),
+  Eigen::Matrix<Real, 3, 2, Eigen::RowMajor> matrix;
+  matrix << std::numeric_limits<Real>::signaling_NaN(),
+      std::numeric_limits<Real>::quiet_NaN(),
       -std::numeric_limits<Real>::infinity(),
+      std::numeric_limits<Real>::infinity(),
+      0,
       kMaxValue;
   ShrinkMatrix(matrix, kMaxValue);
-  ASSERT_EQ(0.0, matrix(0.0));
-  ASSERT_NEAR(1.0, matrix(0, 1), kEps);
+  ASSERT_EQ(0.0, matrix(0, 0));
+  ASSERT_EQ(0.0, matrix(0, 1));
   ASSERT_NEAR(-1.0, matrix(1, 0), kEps);
-  ASSERT_LT(0.0, matrix(1, 1));
+  ASSERT_NEAR(1.0, matrix(1, 1), kEps);
+  ASSERT_EQ(0.0, matrix(2, 0));
+  ASSERT_LT(0.0, matrix(2, 1));
 }
 
 TEST(UtilTest, GetNextRandom) {
